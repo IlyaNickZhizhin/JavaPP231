061b2a0fea4b327be392bdde994ebd5c28f70e7a
”Ilya <ilya.jjn@gmail.com>
2023-02-14 16:11:54 +0500

    Task solved

--- src/main/java/PP/config/AppConfig.java
+++ src/main/java/PP/config/AppConfig.java
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -40,25 +43,34 @@ public class AppConfig {
       return dataSource;
    }
 
+   // @Beans для реализации EntityManager
+
+
    @Bean
-   public LocalSessionFactoryBean getSessionFactory() {
-      LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+   public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
+      LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
       factoryBean.setDataSource(getDataSource());
-      
+
+      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      factoryBean.setJpaVendorAdapter(vendorAdapter);
+
       Properties props=new Properties();
       props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
       props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
-      factoryBean.setHibernateProperties(props);
-      factoryBean.setAnnotatedClasses(User.class);
+      factoryBean.setJpaProperties(props);
+      factoryBean.setPackagesToScan("PP.Model");
+
       return factoryBean;
    }
 
    @Bean
-   public HibernateTransactionManager getTransactionManager() {
-      HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-      transactionManager.setSessionFactory(getSessionFactory().getObject());
+   public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+      JpaTransactionManager transactionManager = new JpaTransactionManager();
+      transactionManager.setEntityManagerFactory(entityManagerFactory);
+
       return transactionManager;
    }
+
 }
--- src/main/java/PP/dao/UserDAOImpl.java
+++ src/main/java/PP/dao/UserDAOImpl.java
@@ -1,38 +1,37 @@
 package PP.dao;
 
 import PP.Model.User;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import org.hibernate.query.Query;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
-public class UserDAOImpl implements UserDAO {
+public class UserDAOImpl implements UserDAO{
 
     @Autowired
-    private SessionFactory sessionFactory;
+    private EntityManagerFactory factory;
 
     @Override
     public List<User> getAllUsers() {
-        return sessionFactory.getCurrentSession().createQuery("from User ", User.class).getResultList();
+        return factory.createEntityManager().createQuery("from User", User.class).getResultList();
     }
 
     @Override
     public void saveUser(User user) {
-        sessionFactory.getCurrentSession().saveOrUpdate(user);
+        factory.createEntityManager().merge(user);
     }
 
     @Override
     public User getUser(int id) {
-        return sessionFactory.getCurrentSession().get(User.class, id);
+        return factory.createEntityManager().find(User.class, id);
     }
 
     @Override
     public void deleteUser(int id) {
-        Query<User> query = sessionFactory.getCurrentSession().createQuery("delete from User " +
-                "where id =:userId");
+        Query query = factory.createEntityManager().createQuery("delete from User where id = :userId");
         query.setParameter("userId", id);
         query.executeUpdate();
     }
